370aebc3e938fff35281d62880c407c79e02a59d
Alihan <amanzholalihan1302@icloud.com>
2026-06-11 20:13:51 +0500

    Module 25: Add SMTP email settings and Django password reset flow

--- goo/goo/urls.py
+++ goo/goo/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -25,5 +26,9 @@ urlpatterns = [
     path('show-warning-message/', views.show_warning_message, name='show_warning_message'),
     path('register/', views.register, name='register'),
     path('profiles/', views.profile_list, name='profile_list'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
     path('admin/', admin.site.urls),
 ]
